7d112c90fbab0938a6e7e58053bb433e01731e6a
Todd Burlington <05tb@users.noreply.github.com>
2020-01-17 20:00:54 +0000

    Update for personal laptop config

--- ansible-install.sh
+++ ansible-install.sh
@@ -5,8 +5,9 @@ sudo easy_install pip
 sudo pip install ansible
 cd ~/Documents/git-repos/
 mkdir mac-setup && cd mac-setup
-git clone git@github.com:05tb/mac-dev-playbook.git
+git clone https://github.com/05tb/mac-dev-playbook.git
 cd mac-dev-playbook
+git checkout personal
 ansible-galaxy install -r requirements.yml
 sed -i "" 's/- which/# - which/' roles/gantsign.visual-studio-code-extensions/vars/pkg-mgr/default.yml
 sudo chown -R $(whoami) /usr/local/lib/pkgconfig
